eb5b4e2fd788b40d517648e239970d18e8db0b90
JIYA <jiya@JIYAs-MacBook-Air.local>
2020-08-29 00:46:24 +0530

    Created NLP model using word embedding

--- spam_detection.py
+++ spam_detection.py
@@ -8,23 +8,32 @@ from tensorflow.keras.layers import LSTM, Embedding, Dense, GlobalMaxPool1D
 import matplotlib.pyplot as plt
 import pickle
 
+#Reading csv file
 df = pd.read_csv('spam_or_not_spam.csv')
 print(df.head())
-df = df.dropna()
 
+df = df.dropna()
+print(df.head())
+
+#Split data for training and testing
 X = df['email']
 y = df['label'].values
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
 
 MaxVocabSize = 2000
 tokenizer = Tokenizer(num_words=MaxVocabSize)
+
+#Preparing vocabulary
 tokenizer.fit_on_texts(x_train)
+
+#Converting text into integer sequences
 sequencesTrain = tokenizer.texts_to_sequences(x_train)
 sequencesTest = tokenizer.texts_to_sequences(x_test)
 
 V = len(tokenizer.word_index)
 print(V)
 
+#Padding to prepare sequences of same length
 maxLen = 50
 X_train = pad_sequences(sequencesTrain, maxlen = maxLen)
 print(X_train.shape)
@@ -39,15 +48,18 @@ model = Sequential([
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 r = model.fit(X_train, y_train, epochs = 10, batch_size = 32, validation_data=(X_test, y_test))
 
+#Plotting loss per iteration
 plt.plot(r.history['loss'], label = 'Loss')
 plt.plot(r.history['val_loss'], label = 'Val_loss')
 plt.legend()
 print(plt.show())
 
+#Plotting accuracy per iteration
 plt.plot(r.history['accuracy'], label = 'Accuracy')
 plt.plot(r.history['val_accuracy'], label = 'Val_accuracy')
 plt.legend()
 print(plt.show())
 
+#Saving the model
 model.save('spam_detection.h5')
 pickle.dump(tokenizer, open("tokenizer.pickle", "wb"))
